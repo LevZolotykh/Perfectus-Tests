be8a409d7e4f60aff0d523814c6fb8ee96166b8c
LevZolotykh <levzolotykh@mail.ru>
2024-11-17 14:11:01 +0300

    VisitUs restyled

--- src/components/UI/VisitUs.jsx
+++ src/components/UI/VisitUs.jsx
@@ -4,15 +4,22 @@ import { FiMapPin, FiClock } from "react-icons/fi";
 const VisitUs = () => {
   return (
     <section className="px-4 py-8 sm:px-8 md:px-16 lg:px-24">
-      <h2 className="text-2xl sm:text-3xl font-bold mb-4">Посетите нас</h2>
-      <p className="text-gray-600 text-sm sm:text-base md:text-lg mb-6">
+      <h2 className="text-3xl lg:text-4xl leading-tight  mb-4 font-serif">
+        Посетите нас
+      </h2>
+      <p className="text-gray-600 text-sm sm:text-base md:text-lg mb-6 md:w-2/3">
         Мы находимся в удобном месте в центре Приозерска. Найдите нас на 3-м
         этаже бизнес-центра по адресу: улица Калинина, 11. Бесплатная парковка
         доступна.
       </p>
 
       <div className="flex flex-col md:flex-row gap-6">
-        <div className="md:flex-1 p-4 border rounded-lg shadow-md">
+        <div
+          className="
+        // md:flex-1
+        md:w-2/3
+         p-4 border rounded-lg shadow-md"
+        >
           <div className="flex items-center mb-4">
             <FiMapPin className="text-cyan-600 mr-2 text-sm sm:text-base md:text-lg lg:text-xl" />
             <h3 className="font-semibold">Локация</h3>
@@ -36,7 +43,12 @@ const VisitUs = () => {
           </div>
         </div>
 
-        <div className="md:flex-2 p-4 border rounded-lg shadow-md">
+        <div
+          className="
+        // md:flex-2
+        md:w-1/3
+         p-4 border rounded-lg shadow-md"
+        >
           <div className="flex items-center mb-4">
             <FiClock className="text-cyan-600 mr-2 text-sm sm:text-base md:text-lg lg:text-xl" />
             <h3 className="font-semibold">Время работы</h3>
